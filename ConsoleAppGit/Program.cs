46b540a965ca913934c867b0b09d11db048528d9
Maxim2 <ii7052785@gmail.com>
2022-02-17 00:00:18 +0300

    first line

--- ConsoleAppGit/Program.cs
+++ ConsoleAppGit/Program.cs
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 ﻿double x = double.Parse(Console.ReadLine());
 double y = double.Parse(Console.ReadLine());
-=======
-﻿double x=double.Parse(Console.ReadLine());
-double y= double.Parse(Console.ReadLine());
->>>>>>> 47c6ba5f0cf04659c66486e933fc33cee234b4b9
 double S = x*y;
 Console.WriteLine("Площадь  равна: " + S);
